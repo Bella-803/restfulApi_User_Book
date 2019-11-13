419bcbc61adbbd48070a487d8a1d635953ff09e8
Bella-803 <abstudies803@gmail.com>
2019-11-13 16:07:54 +0200

    create user and Book

---
+++ server/routes/index.js
@@ -0,0 +1,18 @@
+import Users from '../controllers/user';
+import Books from '../controllers/book';
+
+        export default (app) => {
+
+          app.get('/api', (req, res) => res.status(200).send({
+            message: 'Welcome to the BookStore API!',
+          }));
+
+          app.post('/api/users', Users.signUp); // API route for user to signup
+
+         app.post('/api/users/:userId/books',Books.create);
+
+         app.get('/api/books',Books.list);
+
+         app.put('/api/books/:bookId',Books.modify);
+
+        };
